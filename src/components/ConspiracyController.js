f6b47e56a38935e966ba76b87224b744b8678993
SaihajMann <saihajmann07@gmail.com>
2025-07-09 16:51:21 -0600

    default size change

--- src/components/ConspiracyController.js
+++ src/components/ConspiracyController.js
@@ -142,7 +142,7 @@ const ConspiracyController = () => {
 
   const add1To100 = () => {
     var num = 1
-    while(num <= 1000){
+    while(num <= 300){
       addNodeWithID(num)
       num +=1
     }
